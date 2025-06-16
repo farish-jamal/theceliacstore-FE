508a9c9c5e3b489d90769fd506fc17d2328aa000
Kashif <kashifdeshmukh786@gmail.com>
2025-06-17 00:39:14 +0530

    Update BACKEND_URL to use HTTPS for secure connections

--- app/constants/URL.ts
+++ app/constants/URL.ts
@@ -1,2 +1,2 @@
 // export const BACKEND_URL = "http://localhost:8000" as const;
-export const BACKEND_URL = "http://theceliacstore-be.onrender.com" as const;
+export const BACKEND_URL = "https://theceliacstore-be.onrender.com" as const;
