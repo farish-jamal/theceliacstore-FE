3f8ac1aeaa6668c8036a33382ebbff5b4c69864e
Sayem Kalim <101326225+sayemkalim@users.noreply.github.com>
2025-11-22 15:00:05 +0530

    chore(URL): Update backend URL constant for staging environment

--- app/constants/URL.ts
+++ app/constants/URL.ts
@@ -1,2 +1,2 @@
 // export const BACKEND_URL = "http://localhost:8000" as const;
-export const BACKEND_URL = "https://theceliacstore-be.onrender.com" as const;
+export const BACKEND_URL = "https://theceliacstore-be-7as7.onrender.com" as const;
