04c3b7c8e3e79a6b52dffb6d0351c299efa4d52a
farish-jamal <farishjamal98@gmail.com>
2025-12-10 14:50:43 +0530

    chore(URL): Update backend URL constant for local development environment

--- app/constants/URL.ts
+++ app/constants/URL.ts
@@ -1,3 +1,3 @@
-// export const BACKEND_URL = "http://localhost:8000" as const;
+// export const BACKEND_URL = "http://localhost:5000" as const;
 // export const BACKEND_URL = "https://theceliacstore-be-7as7.onrender.com" as const;
 export const BACKEND_URL = "https://theceliacstore-be.onrender.com" as const;
